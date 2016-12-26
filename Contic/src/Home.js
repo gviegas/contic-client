4deb61ee95e0760da8d7d78ae957975d44d62808
Gustavo Viegas <gviegas@users.noreply.github.com>
2016-12-26 20:36:12 -0200

    Icon and bar labels created

---
+++ Contic/src/Home.js
@@ -0,0 +1,18 @@
+//
+// Created by Gustavo Viegas on 2016/12
+//
+
+import React, { Component } from 'react'
+import menuLabel from './img/menu-label.svg'
+import searchLabel from './img/search-label.svg'
+import './Home.css'
+
+class Home extends Component {
+    render() {
+        return (
+            <div />
+        );
+    }
+}
+
+export default Home;
