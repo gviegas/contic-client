1b366ad893b7c2d2687e52de2f3244b7cada06e8
Gustavo Viegas <gviegas@users.noreply.github.com>
2017-01-01 20:05:28 -0200

    GMap, Marker and InfoWindow integration done

--- Contic/src/GMap.js
+++ Contic/src/GMap.js
@@ -175,57 +175,6 @@ const info = {
   u3 : {position : {lat : 64.431290, lng : -20.0094541}, content : 'unit u3'}
 };
 
-class _Instance {
-  constructor() { 
-    this.map = null;
-    this.infoWindow = null;
-    this.initMap = this.initMap.bind(this);
-  }
-
-  initMap() {
-    this.createMap({lat : 64.9312715, lng : -19.0144547});
-    this.createInfoWindow();
-    for(let entry in info) {
-      this.createMarker(info[entry]['position'], info[entry]['content']);
-    }
-  }
-
-  createMap(position) {
-    if(this.map) { return; }
-    // eslint-disable-next-line
-    this.map = new google.maps.Map(document.getElementById('MapInstance'),
-    {
-      center : position,
-      zoom : 6,
-      styles : style,
-      disableDefaultUI : true
-    });
-  }
-
-  createMarker(position, content) {
-    if(!this.map) { return; }
-    // eslint-disable-next-line
-    var marker = new google.maps.Marker({
-      position : position,
-      map : this.map
-    });
-    marker.addListener('click', () => {
-      this.infoWindow.setContent(content);
-      this.infoWindow.open(this.map, marker);
-    });
-  }
-
-  createInfoWindow() {
-    if(this.infoWindow) { return; }
-    // eslint-disable-next-line
-    this.infoWindow = new google.maps.InfoWindow({});
-  }
-}
-
-
-////////////////////
-////////////////////
-
 
 class InfoWindow extends Component {
   constructor(props) {
@@ -233,16 +182,15 @@ class InfoWindow extends Component {
     this.open = this.open.bind(this);
     // eslint-disable-next-line
     this.infoWindow = new google.maps.InfoWindow({});
-    //this.open(); // test
   }
 
   open() {
-    this.infoWindow.setContent(this.props.content);
-    this.infoWindow.open(this.props.map, this.props.marker);
+    this.infoWindow.setContent(this.props.data['content']);
+    this.infoWindow.open(this.props.map, this.props.data['marker']);
   }
 
   render() {
-    if(this.props.open) { this.open(); }
+    if(this.props.data['marker']) { this.open(); }
     return null;
   }
 }
@@ -255,13 +203,13 @@ class Marker extends Component {
       position : this.props.position,
       map : this.props.map
     });
-    this.marker.addListener('click', () => { this.setState({click : true}); });
-    this.state = {click : false};
+    this.marker.addListener('click', () => { 
+      this.props.onClick({marker : this.marker, content : this.props.content});
+    });
   }
+
   render() {
-    return (
-      <InfoWindow open={this.state.click} content={'qwerty'} marker={this.marker} map={this.props.map} />
-    );
+    return null;
   }
 }
 
@@ -279,10 +227,31 @@ class GMap extends Component {
       styles : style,
       disableDefaultUI : true      
     });
+    this.handleMarkerClick = this.handleMarkerClick.bind(this);
+    this.state = { markerClick : { marker : null, content : '' } };
   }
+  
+  handleMarkerClick(data) {
+    this.setState({ markerClick : data});
+  }
+
   render() {
+    let markers = [];
+    for(let key in info) {
+      markers.push(
+        <Marker key={key}
+        onClick={this.handleMarkerClick} 
+        data={this.state.markerClick} 
+        position={info[key]['position']}
+        content={info[key]['content']} 
+        map={this.map} />
+      );
+    }
     return (
-      <Marker position={position} map={this.map} />
+      <div className="GMap">
+        {markers}
+        <InfoWindow data={this.state.markerClick} map={this.map} />
+      </div>
     );
   }
 }
