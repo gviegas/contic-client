e2819eaf37cd12af1e7696921ec229323cabd4a2
Gustavo Viegas <gviegas@users.noreply.github.com>
2017-01-03 20:17:24 -0200

    Simple bar chart done

--- Contic/src/App.js
+++ Contic/src/App.js
@@ -5,13 +5,14 @@
 import React, { Component } from 'react';
 import SignIn from './SignIn';
 import Home from './Home';
+import Chart from './Chart';
 import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+      <Chart />
       </div>
     );
   }
