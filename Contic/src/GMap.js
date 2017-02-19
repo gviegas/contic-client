8ccb4c78bf1f95533c55e18c6ec140dea21cd612
Gustavo Viegas <gviegas@users.noreply.github.com>
2017-02-19 16:21:38 -0300

    GHeatMap base functionality done

--- Contic/src/GMap.js
+++ Contic/src/GMap.js
@@ -4,179 +4,10 @@
 
 import React, { Component } from 'react';
 import client from './Client';
+import { MAP_STYLE } from './Defs';
 import './css/GMap.css';
 
-const style = 
-[
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#f5f5f5"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ffffff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#dadada"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.line",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#e5e5e5"
-      }
-    ]
-  },
-  {
-    "featureType": "transit.station",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#eeeeee"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#c9c9c9"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  }
-];
-
-const position = {lat : 40.635364, lng : -73.961016};
-// const position = {lat : 64.9312715, lng : -19.0144547};
-
-// const info = {
-//   u1 : {position : {lat : 63.9312120, lng : -19.2244528}, content : 'unit u1'},
-//   u2 : {position : {lat : 64.9013208, lng : -19.1444537}, content : 'unit u2'},
-//   u3 : {position : {lat : 64.431290, lng : -20.0094541}, content : 'unit u3'}
-// };
-
+const position = {lat : 40.634185, lng : -73.962016};
 
 class InfoWindow extends Component {
   constructor(props) {
@@ -244,8 +75,8 @@ class GMap extends Component {
     // eslint-disable-next-line
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: position,
-      zoom: 16,
-      styles: style,
+      zoom: 17,
+      styles: MAP_STYLE,
       disableDefaultUI: true      
     });
 
